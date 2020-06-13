09ce453d94bb2236c700da9c6a140e18b5b3f1f1
Tanapak Ngamsappakit <tanapakngamsuppaki@gmail.com>
2020-06-13 16:38:16 +0700

    1

---
+++ 4-digital/4-digital.ino
@@ -0,0 +1,26 @@
+#include "TM1637.h"
+
+const int CLK = 17;
+const int DIO = 16;
+
+TM1637 sevenSegment(CLK,DIO);
+
+void setup()
+{
+    sevenSegment.init();
+    sevenSegment.set(7);//bright0-7
+    sevenSegment.displayNum(10);
+    delay(2000);
+    sevenSegment.displayNum(1234);
+    delay(2000);
+    sevenSegment.displayStr("OFF");
+    delay(2000);
+
+}
+int number = 0;
+void loop()
+{
+    sevenSegment.displayNum(number++);
+    delay(100);
+
+}
---
+++ DS1302/DS1302.ino
@@ -0,0 +1,53 @@
+#include "DS1302.h"
+
+
+// Init the DS1302
+// DS1302 rtc([CE/RST], [I/O], [CLOCK]);
+DS1302 rtc(8, 7, 6);
+
+void clockdate() 
+
+{
+
+       
+
+        Serial.println(rtc.getDateStr(FORMAT_LONG,FORMAT_LITTLEENDIAN, '/'));
+
+        Serial.println(rtc.getDOWStr());
+
+        Serial.println(rtc.getTimeStr());
+
+}
+ 
+void setup()
+
+{
+
+  Serial.begin(9600);
+
+  // ตั้งเวลาครั้งแรก เอา comment นี้ออก ถ้าตั้งเสร็จแล้ว comment นี้ไว้เพื้อให้เวลาเดินต่อ
+
+  rtc.halt(false);
+
+  rtc.writeProtect(false);
+
+  rtc.setDOW(SATUARDAY);         
+
+  rtc.setTime(16, 36,00);      
+
+  rtc.setDate(13, 6, 2020);    
+
+  rtc.writeProtect(true);
+  }
+
+ 
+
+void loop()
+
+{ 
+
+  clockdate();
+
+  delay(1000);
+
+}
