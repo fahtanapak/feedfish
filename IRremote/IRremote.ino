d0d4a4522eb2050979ae00e810b947e75981f85c
Tanapak Ngamsappakit <tanapakngamsuppaki@gmail.com>
2020-06-14 17:52:22 +0700

    Remote

--- IRremote/IRremote.ino
+++ IRremote/IRremote.ino
@@ -1,48 +1,51 @@
 #include "IRremote.h"
 
-int receiver = 11; // Signal Pin of IR receiver to Arduino Digital Pin 11
-
-/*-----( Declare objects )-----*/
+int receiver = 11; 
 IRrecv irrecv(receiver);     // create instance of 'irrecv'
 decode_results results;      // create instance of 'decode_results'
 
-void setup()   /*----( SETUP: RUNS ONCE )----*/
+void setup()   
 {
   Serial.begin(9600);
   Serial.println("IR Receiver Button Decode"); 
   irrecv.enableIRIn(); // Start the receiver
 
-}/*--(end setup )---*/
+}
 
 
-void loop() 
+void loop()   
 {
   if (irrecv.decode(&results)) // have we received an IR signal?
 
   {
+    translateIR(); 
     irrecv.resume(); // receive the next value
-  }
-switch(results.value)
+  }  
+}
+
+
+void translateIR() 
+{
+  switch(results.value)
 
   {
-
-  case 0xFF629D: Serial.println(" FORWARD"); break;
-  case 0xFF22DD: Serial.println(" LEFT");    break;
-  case 0xFF02FD: Serial.println(" -OK-");    break;
-  case 0xFFC23D: Serial.println(" RIGHT");   break;
-  case 0xFFA857: Serial.println(" REVERSE"); break;
-  case 0xFF6897: Serial.println(" 1");    break;
-  case 0xFF9867: Serial.println(" 2");    break;
-  case 0xFFB04F: Serial.println(" 3");    break;
-  case 0xFF30CF: Serial.println(" 4");    break;
-  case 0xFF18E7: Serial.println(" 5");    break;
-  case 0xFF7A85: Serial.println(" 6");    break;
-  case 0xFF10EF: Serial.println(" 7");    break;
-  case 0xFF38C7: Serial.println(" 8");    break;
-  case 0xFF5AA5: Serial.println(" 9");    break;
-  case 0xFF42BD: Serial.println(" *");    break;
-  case 0xFF4AB5: Serial.println(" 0");    break;
-  case 0xFF52AD: Serial.println(" #");    break;
+  case 0xFFA25D: Serial.println(" 1");    break;
+  case 0xFF629D: Serial.println(" 2"); break;
+  case 0xFFE21D: Serial.println(" 3");    break;
+  case 0xFF22DD: Serial.println(" 4");    break;
+  case 0xFF02FD: Serial.println(" 5");    break;
+  case 0xFFC23D: Serial.println(" 6");   break;
+  case 0xFFE01F: Serial.println(" 7");    break;
+  case 0xFFA857: Serial.println(" 8"); break;
+  case 0xFF906F: Serial.println("9");    break;
+  case 0xFF6897: Serial.println(" *");    break;
+  case 0xFF9867: Serial.println(" 0");    break;
+  case 0xFFB04F: Serial.println(" #");    break;
+  case 0xFF10EF: Serial.println("Left");    break;
+  case 0xFF5AA5: Serial.println("Right");    break;
+  case 0xFF18E7: Serial.println("UP");    break;
+  case 0xFF4AB5: Serial.println("DOWN");    break;
+  case 0xFF38C7: Serial.println("OK");    break;
   case 0xFFFFFFFF: Serial.println(" REPEAT");break;  
 
   default: 
@@ -51,6 +54,25 @@ switch(results.value)
   }// End Case
 
   delay(500); // Do not get immediate repeat
+
+
+}
+//ทดสอบรหัส
+ /* #include "IRremote.h"
+
+const int RECV_PIN = 11;
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+
+void setup(){
+  Serial.begin(9600);
+  irrecv.enableIRIn();
 }
 
-  
+void loop(){
+  if (irrecv.decode(&results)){
+        Serial.println(results.value, HEX);
+        irrecv.resume();
+  }
+}
+*/
